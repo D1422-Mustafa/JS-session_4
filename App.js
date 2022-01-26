8ec5cf01c4793e43a57e4591d5722b73cb11da4c
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-01-27 00:01:45 +0300

    session 4 işlendi

---
+++ App.js
@@ -0,0 +1,118 @@
+// Conditionals
+
+// let score = 48;
+
+// let score = +prompt('Notunuzu girin : ')
+
+// let grade = score >= 50;
+
+// console.log(grade);
+
+// if (grade) console.log(Tebrikler, notunuz ${score} geçtiniz);
+
+// console.log('kod sonu');
+
+// let score = 75;
+// if (score>=50) {
+//     if (score > 80); {
+//         console.log('Tebrikler çok başarılı öğrencisiniz');
+//     }else{
+//         console.log('Tebrikler geçtiniz');
+//     }
+   
+       
+// } else {
+//     console.log('Üzgünüm kaldınız');
+   
+// }
+
+// var score = 85;
+// // score >=50 ? console.log('tebrikler geçtiniz'): console.log('üzgünüm kaldınız');
+
+// score >= 80 ? console.log('Tebrikler çok başarılı bir öğrencisiniz'): (score >=50 ? console.log('tebrikler geçtiniz'): console.log('üzgünüm kaldınız'));
+
+// var text ;
+// var fruits = prompt('Enter your favorite fruit :')
+
+// //toLoWercase()
+
+// switch (fruits.toLowerCase()) {
+//     case "banana":
+//         text = "Banana is good."
+//         break;
+//     case "lime" :
+//     case "lemon" :
+//     case "orange":
+//         text = `I am not a fan of ${fruits.toLowerCase()}.`;
+//         break;
+//     case "apple":
+//         text = "Banana is How you like them apples?"
+//         break;
+//     default:
+//         text = "I have never heard of that fruit..."
+//         break;
+// }
+
+// console.log(text);
+
+// ÖDEV
+
+// var text ;
+// var days = prompt('Enter day :')
+
+// switch (days.toLowerCase()) {
+//     case "monday":
+//     case "wednesday" :
+//     case "thursday" :
+//     case "saturday" :
+//         text ="in-class" ;
+//         break;
+//     case "tuesday" :
+//     case "friday" :
+//         text = "team work" ;
+//         break;
+//     case "sunday" :
+//         text = "sunday is empty day";
+//     default :
+//         text= "wrong day" ;
+//         break;
+// }
+
+// console.log(text);
+
+//leap year------------
+
+// var year = 2152;
+// var month = 2;
+// var dayCount;
+// switch (month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         dayCount = 31;
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         dayCount = 30;
+//         break;
+//     case 2:
+//         if (((year % 4 == 0) && !(year % 100 == 0))
+//             || (year % 400 == 0))
+//             dayCount = 29;
+//         else
+//             dayCount = 28;
+//         break;
+//     default:
+//         dayCount = -1; // invalid month
+// }
+// console.log(dayCount); // 29
+
+
+//leap year --------------<<<<<<<<<<<
+
---
+++ loops.js
@@ -0,0 +1,56 @@
+// let i = 0 ;
+// let sum = 0;
+// while (i < 6) {
+//     sum += i
+//     i++
+// }
+// console.log(sum);
+
+// let i = 10 ;
+// let sum = 0;
+
+// do {
+//     sum += i
+//     i++
+// } while (i < 6);
+// console.log({sum}, {i});
+
+// let i = +prompt('Bir sayı giriniz :')
+
+// // console.log(isNaN(i), {i});
+
+
+// while (isNaN(i)) {
+//     i = +prompt("Bir sayı giriniz :")
+// }
+
+// console.log('Bir sayı girdiniz');
+
+// let i = +prompt('Bir sayı giriniz :')
+// while (isNaN(i)) {
+//         i = +prompt("Bir sayı giriniz :")
+// }   
+//     console.log('Bir sayı girdiniz');
+
+// let i;
+
+// do {
+//     let i = +prompt('Bir sayı giriniz :')
+// } while (isNaN(i));
+
+// console.log(`Girilen değer ${i}`);
+
+// let i = 0;
+
+// while (i>6) {
+//     console.log(i);
+//     i++
+//     if (i==6) break;  
+// }
+
+// let i = 0;
+// for (let i = 0; i < 10; i++ ) {
+//     console.log(i);
+    
+// }
+// console.log({i});
