981a7c898068f9c7f78844f1224003146cfac138
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-01-29 13:20:53 +0300

    yeni örnekler eklendi

--- loops.js
+++ loops.js
@@ -54,3 +54,57 @@
     
 // }
 // console.log({i});
+
+// let str = 'Clarusway'
+// console.log(str[str.length-1]);
+// // console.log(str.length);
+// // console.log(str[0]);
+
+// for (let i = 0; i < str.length; i++){
+//     console.log( {i}, str[i]);
+// }
+
+
+// for (let i =1; i <= 100; i++) {
+//     if (i % 5 ===0 ) break;
+//     console.log(i);
+// }
+
+// while (true) {
+//     let x = prompt(`Bir sayı giriniz: \n q ile çıkış yapınız`);
+//     if (x == 'q') {
+//         console.log('Çıkış yapıldı');
+//         break
+//     } else if (isNaN(x)){
+//         continue;
+//     } else {
+//         console.log(`${x}'in karesi = ${x*x}`);
+//     }
+// }
+
+// console.log(Math.trunc(Math.random()*6)+1);
+
+let x, y ;
+let count = 0;
+let cift = 0;
+while (true) {
+    x = Math.trunc(Math.random()*6)+1;
+    y = Math.trunc(Math.random()*6)+1;
+
+    if (x ==6 && y == 6){
+        count++
+        console.log(x, y, 'Kazandınız');
+        break
+    }else if (x == y && x!=6) {
+        cift++
+        console.log(x, y, `${cift}. cift zar`);
+        if (cift == 3){
+            console.log('Kaybettiniz');
+            break
+        }
+    }
+    console.log(x, y);
+    count++;
+}
+
+console.log(count);
